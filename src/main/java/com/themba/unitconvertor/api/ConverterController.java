8540d00a4bee006322846f86614271e5640613b8
Themba <tfmzworld@gmail.com>
2020-06-14 04:33:01 +0200

    Add validation

--- src/main/java/com/themba/unitconvertor/api/ConverterController.java
+++ src/main/java/com/themba/unitconvertor/api/ConverterController.java
@@ -1,9 +1,12 @@
 package com.themba.unitconvertor.api;
 
 import com.themba.unitconvertor.service.ConverterService;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.NotNull;
 
 @RestController
 @RequestMapping("conversions")
@@ -16,22 +19,22 @@ public class ConverterController {
 	}
 
 	@RequestMapping("ktoc")
-	public double ConvertKelvinToCelsius(@RequestBody double value){
+	public double ConvertKelvinToCelsius(@RequestBody @Validated @NotNull double value){
 		return converterService.ConvertKelvinToCelsius(value);
 	}
 
 	@RequestMapping("ctok")
-	public double ConvertCelsiusKelvin(@RequestBody double value){
+	public double ConvertCelsiusKelvin(@RequestBody @Validated @NotNull double value){
 		return converterService.ConvertCelsiusToKelvin(value);
 	}
 
 	@RequestMapping("mtok")
-	public double ConvertMilesToKilometers(@RequestBody double value){
+	public double ConvertMilesToKilometers(@RequestBody @Validated @NotNull double value){
 		return converterService.ConvertMilesToKilometers(value);
 	}
 
 	@RequestMapping("ktom")
-	public double ConvertKilometersToMiles(@RequestBody double value) {
+	public double ConvertKilometersToMiles(@RequestBody @Validated @NotNull double value) {
 		return converterService.ConvertKilometersToMiles(value);
 	}
 }
