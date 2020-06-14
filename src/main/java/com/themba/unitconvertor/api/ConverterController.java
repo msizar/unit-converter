ebb0302d22d6da22d708214400bdeb70d3a8c1a3
Themba <tfmzworld@gmail.com>
2020-06-14 04:26:17 +0200

    Create ConverterController.java

---
+++ src/main/java/com/themba/unitconvertor/api/ConverterController.java
@@ -0,0 +1,27 @@
+package com.themba.unitconvertor.api;
+
+import com.themba.unitconvertor.service.ConverterService;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("conversions")
+public class ConverterController {
+
+	private  final ConverterService converterService;
+
+	public ConverterController(ConverterService converterService) {
+		this.converterService = converterService;
+	}
+
+	@RequestMapping("ktoc")
+	public double ConvertKelvinToCelsius(@RequestBody double value){
+		return converterService.ConvertKelvinToCelsius(value);
+	}
+
+	@RequestMapping("ctok")
+	public double ConvertCelsiusKelvin(@RequestBody double value){
+		return converterService.ConvertCelsiusToKelvin(value);
+	}
+}
